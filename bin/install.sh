8747d5159fa65e3599e9bf10ecc0c28b3dbc4c87
aurelien <aurelien@paris-server.home>
2019-03-15 10:19:56 +0000

    [+] Add the delete of the default sites in nginx config

--- bin/install.sh
+++ bin/install.sh
@@ -4,4 +4,5 @@ sudo apt install -y nginx=1.14.0-0ubuntu1.2
 sudo cp ../conf/nginx.conf /etc/nginx
 sudo cp ../conf/avandaele.fr /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/avandaele.fr /etc/nginx/sites-enabled
+sudo rm /etc/nginx/sites-enabled/default
 sudo systemctl start nginx

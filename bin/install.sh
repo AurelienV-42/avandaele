224fd006cb63cd5616a75ffc7d4d09fce4df11b7
Aurélien Vandaële <aurelien.vandaele@epitech.eu>
2019-03-15 11:23:26 +0100

    [+] Clean url and update script to install

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 sudo apt install -y nginx=1.14.0-0ubuntu1.2
+
+sudo rm /etc/nginx/nginx.conf
+sudo rm /etc/nginx/nginx/sites-available/avandaele.fr
+sudo rm /etc/nginx/nginx/sites-enabled/avandaele.fr
+sudo rm /etc/nginx/sites-enabled/default
+
 sudo cp ../conf/nginx.conf /etc/nginx
 sudo cp ../conf/avandaele.fr /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/avandaele.fr /etc/nginx/sites-enabled
-sudo rm /etc/nginx/sites-enabled/default
+
 sudo systemctl start nginx
